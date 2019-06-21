14366a2bc0a477374738e7bc45cc616a73fa57a8
Siddharth Srivatsa <siddharth.srivatsa@quintype.com>
2019-06-21 16:41:54 +0530

    Tamil Spellchecker

---
+++ routes/checkspell.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+
+function handleResponse(initialWords)  {
+  var data = JSON.parse(initialWords);
+  return data.map(word=>{
+    if(word['Suggestions']!=="")  return {'word':word['Userword'],'suggestions':word['Suggestions']};
+  }).filter((element)=>element);
+}
+/* POST checkspell listing. */
+router.post('/', function(req, res) {
+  var options = { method: 'POST',
+  url: 'http://vaani.neechalkaran.com/checkspell',
+  headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+  body: 'action=vaani&tamilwords='+req.body.tamilwords 
+};
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    res.send(handleResponse(body));
+  });
+});
+
+/* GET checkspell listing */
+router.get('/', function(req, res) {
+    res.send('respond with a resource'+toString(req));
+});
+
+module.exports = router;
+
+
+
