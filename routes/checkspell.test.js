2830bcfca914d05bf153dde7493286b584aa16cc
Siddharth Srivatsa <siddharth.srivatsa@quintype.com>
2019-07-02 17:53:48 +0530

    updated tests

--- routes/checkspell.test.js
+++ routes/checkspell.test.js
@@ -7,7 +7,7 @@ describe("Spellcheck API", () => {
     const response = await request(app)
       .post("/api/spell-check/tamil")
       .send(
-        "action=vaani&tamilwords=அவரிடமிருந்து|உற்சாகமாகப்|பதில்கள்|வருகின்றன|அரஙகேறி|முடிந்துள்ள|நிலையில்"
+        "words=அவரிடமிருந்து|உற்சாகமாகப்|பதில்கள்|வருகின்றன|அரஙகேறி|முடிந்துள்ள|நிலையில்"
       )
       .expect(200);
     expect(response.body).toEqual([
